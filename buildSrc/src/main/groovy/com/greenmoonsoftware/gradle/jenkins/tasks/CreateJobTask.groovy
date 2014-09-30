d196922caa7ca1fc4702c55737a81ab4ca51f4b0
Robert G. Greathouse <greathouse.robert@gmail.com>
2014-09-29 20:18:42 -0500

    Add initial createJob task

--- buildSrc/src/main/groovy/com/greenmoonsoftware/gradle/jenkins/tasks/CreateJobTask.groovy
+++ buildSrc/src/main/groovy/com/greenmoonsoftware/gradle/jenkins/tasks/CreateJobTask.groovy
@@ -1,11 +1,28 @@
 package com.greenmoonsoftware.gradle.jenkins.tasks
 
+import com.greenmoonsoftware.gradle.jenkins.ConfigGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class CreateJobTask extends DefaultTask {
     @TaskAction
     def createJob() {
+        def url = 'http://localhost:8080'
+        //http://localhost:8080/createItem
+        def configXml = ConfigGenerator.generate(project)
+        String type = "application/xml";
+        URL u = new URL("${url}/createItem?name=${project.name}");
+        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty( "Content-Type", type );
+        conn.setRequestProperty( "Content-Length", String.valueOf(configXml.length()));
+        OutputStream os = conn.getOutputStream();
+        os.write(configXml.getBytes());
+        os.flush()
+        os.close()
+        def responseCode = conn.responseCode
 
+        println "Done: ${responseCode}"
     }
 }
